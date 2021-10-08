774d7d0ceb83c99bbc5a71db1b75dd09c7e59e14
nik <nikandr33@inbox.ru>
2021-10-08 11:09:56 +0300

    hotfix

--- upload/admin/model/extension/module/menu_constructor_nik.php
+++ upload/admin/model/extension/module/menu_constructor_nik.php
@@ -137,7 +137,7 @@ class ModelExtensionModuleMenuConstructorNik extends Model {
         if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
             $sql .= " ORDER BY " . $data['sort'];
         } else {
-            $sql .= " ORDER BY mid.name";
+            $sql .= " ORDER BY mi.sort_order";
         }
 
         if (isset($data['order']) && ($data['order'] == 'DESC')) {
